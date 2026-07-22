c4a15d37fc7bc5dab7d9da8c6b910df78aa6dcd8
Raymond <raymondtdev@gmail.com>
2026-07-22 16:24:09 +0200

    initial typescript setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
+import "dotenv/config";
 import express from "express";
+import { Resend } from "resend";
 
 const app = express();
 const PORT = 3000;
+
+const resend: Resend = new Resend(process.env.RESEND_API_KEY);
 
 app.get('/', (req, res) => {
   res.send(`<h1>Hello from the server</h1>`);
